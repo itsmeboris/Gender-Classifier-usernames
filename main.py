e1a0fbdcb0ef1c1ffee7466a65132478ae758e73
itsmeboris <boris.sobol@gmail.com>
2021-06-08 10:35:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from utility import *
 epochs = 200
 models = model_types
 model_name = 'gru_best'
-# for train_name in all_sets:
-#     print(f'Starting to train on {train_name}')
-#     models = []
-#     df = load_split_data(train_name, debug=False, split=True)
-#     train, test = train_test_split(df, train_size=0.8)
-#     cnn = VectorCnn(f'{train_name}_{model_name}', epochs, debug=False)
-#     cnn.build_model(model_name)
-#     cnn.fit_model(train, test)
-#     models.append(cnn)
-#     save_best_model(models, train_name + f'_{model_name}')
+for train_name in all_sets:
+    print(f'Starting to train on {train_name}')
+    models = []
+    df = load_split_data(train_name, debug=False, split=True)
+    train, test = train_test_split(df, train_size=0.8)
+    cnn = VectorCnn(f'{train_name}_{model_name}', epochs, debug=False)
+    cnn.build_model(model_name)
+    cnn.fit_model(train, test)
+    models.append(cnn)
+    save_best_model(models, train_name + f'_{model_name}')
 
 for test_name in all_sets[3:]:
     models = []
@@ -22,8 +22,7 @@ for test_name in all_sets[3:]:
     df = load_split_data(test_name, debug=False, split=True)
     train, test = train_test_split(df, train_size=0.8)
 
-    stacked = StackedEnsemble([set + f'_{model_name}' for set in all_sets], f'stacked_ensemble_{test_name}_{model_name}', epochs, debug=True)
-    # stacked = StackedEnsemble(list(set(all_sets) - {test_name}), f'stacked_ensemble_{i}', epochs)
+    stacked = StackedEnsemble([set + f'_{model_name}' for set in all_sets], f'stacked_ensemble_{test_name}_{model_name}', epochs, debug=False)
     stacked.build_model()
     stacked.fit_model(train, test)
     models.append(stacked)
