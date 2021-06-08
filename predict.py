f6057e96d3bfda9b10d790d615e86b7a7b11f28e
itsmeboris <boris.sobol@gmail.com>
2021-06-08 10:32:41 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -21,6 +21,8 @@ def vec_generator(df, max_len, vectors, batch_size=64):
 
 
 results = {}
+print('Starting prediction')
+print('-' * 80)
 for model_name in ['default', 'LSTM_CONV', 'gru_best', 'bid_GRU_bid_LSTM', 'bidirectional_LSTM']:
     # load the stacked models
     stacked_models = load_all_models([f'stacked_ensemble_{name}_{model_name}' for name in all_sets])
@@ -33,14 +35,19 @@ for model_name in ['default', 'LSTM_CONV', 'gru_best', 'bid_GRU_bid_LSTM', 'bidi
         df[f'{model_name}_{name}'] = get_labels(
             model.predict(vec_generator(df, max_len, vectors, batch_size), batch_size, verbose=0,
                           steps=(len(df) // batch_size) + 1))
+        print(f'finished predicting: {model_name}_{name}')
     # perform ensemble of all the data
     df[f'ensemble_{model_name}'] = df.apply(lambda row: ensemble_label(row, f'{model_name}_'), axis=1)
+    print('predicted ensemble')
     for stacked_model, name in zip(stacked_models, all_sets):
         # predict using the stacked model
         df[f'stacked_{model_name}_{name}'] = get_labels(
             stacked_model.predict(vec_generator(df, max_len, vectors, batch_size), batch_size, verbose=0,
                                   steps=(len(df) // batch_size) + 1))
+        print(f'finished predicting: stacked_{model_name}_{name}')
     df[f'ensemble_stacked_{model_name}'] = df.apply(lambda row: ensemble_label(row, f'stacked_{model_name}_'), axis=1)
+    print('predicted stacked ensemble')
     df.to_csv('datasets/email_list_predicted.csv', index=False)
+    print('-' * 80)
 
 df.to_csv('datasets/email_list_predicted.csv', index=False)
